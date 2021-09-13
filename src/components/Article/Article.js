139873cdbc119afd986d9f7bf799379df4df5b2a
Max Bronner <88713269+max-bronner@users.noreply.github.com>
2021-09-13 16:27:08 +0200

    Format code

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,19 +1,12 @@
 function Article({ content }) {
   return (
     <article className={'article'}>
-      <h2>
-        {content.title}
-      </h2>
-      <p>
-        {content.text}
-      </p>
-      
-      <address>
-        {content.author}
-      </address>
-      
+      <h2>{content.title}</h2>
+      <p>{content.text}</p>
+
+      <address>{content.author}</address>
     </article>
-  )
+  );
 }
 
 export default Article;
--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -2,25 +2,16 @@ import './ArticleCard.css';
 import { Link } from 'react-router-dom';
 
 function ArticleCard({ content }) {
-
-
   return (
     <article className={'article-card'}>
       <Link className={'article-card__link'} to={content.link}>
-        <h2 className={'article-card__title'}>
-          {content.title}
-        </h2>
-        <p className={'article-card__text'}>
-          {content.preview}
-        </p>
-        
-        <address className={'article-card__author'}>
-          {content.author}
-        </address>
-        
+        <h2 className={'article-card__title'}>{content.title}</h2>
+        <p className={'article-card__text'}>{content.preview}</p>
+
+        <address className={'article-card__author'}>{content.author}</address>
       </Link>
     </article>
-  )
+  );
 }
 
 export default ArticleCard;
