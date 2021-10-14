c5baf36df080f7a249c11f3dbb92ae415efd6174
Max Bronner <88713269+max-bronner@users.noreply.github.com>
2021-10-14 13:58:07 +0200

    Track web vitals on navigation

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,27 @@
 const reportWebVitals = (onPerfEntry) => {
+  let observers = [];
+
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+      const handleMetric = (metric) => {
+        metric.navigationType = performance.getEntriesByType('navigation')[0]?.type || 'navigate';
+        onPerfEntry(metric);
+      };
+
+      observers.push(getCLS(handleMetric));
+      observers.push(getFID(handleMetric));
+      observers.push(getFCP(handleMetric));
+      observers.push(getLCP(handleMetric));
+      observers.push(getTTFB(handleMetric));
     });
   }
+
+  return {
+    disconnect: () => {
+      observers.forEach((observer) => observer.disconnect && observer.disconnect());
+      observers = [];
+    },
+  };
 };
 
 export default reportWebVitals;
